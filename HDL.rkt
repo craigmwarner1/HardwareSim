99b8b4606a388111683adb47f34042858cb526eb
Craig Warner <craigmwarner1@gmail.com>
2015-10-08 14:06:06 -0400

    Started design of HDL stuff.

--- HDL.rkt
+++ HDL.rkt
@@ -1,9 +1,25 @@
 #lang racket
-
+(require plai
+	 rackunit)
 ;; One small step for a man, one giant leap for a project Matt has been dreaming-of/dreading.
 
 ;;DATATYPES
 
 (define-type Chip
-  
-  )
+	[IN  (in symbol?)]
+	[OUT (out symbol?)]
+	[PARTS (name Chip?)
+	       (args list?)]
+ )
+
+
+
+;;PARSING
+
+;;parse : s-exp -> AST
+(define (parse sexp)
+
+)
+
+
+;;INTERPRETATION
